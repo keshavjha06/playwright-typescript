3ee8bf53b46fd6401521110fc02eef3a9b851037
Keshav Jha <keshavjha0610@gmail.com>
2024-01-06 14:20:44 +0530

    added traceviewer test

---
+++ tests/traceviewer.spec.ts
@@ -0,0 +1,11 @@
+import test from "@playwright/test";
+
+test('locators demo', async ({ page }) => {
+    await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
+    await page.getByPlaceholder('Password').fill('admin123')
+    await page.getByRole('button', { name: /login/i }).click()
+    await page.getByRole('button', { name: "check" }).click()
+
+})
+
