00701b9db14a013d6cc56b25fe93f4265e00372b
Keshav Jha <keshavjha0610@gmail.com>
2024-01-08 22:34:07 +0530

    updated with comment

--- tests/test-config.spec.ts
+++ tests/test-config.spec.ts
@@ -8,3 +8,5 @@ test.use({
 test('test-config demo', async ({ page }) => {
     await page.goto('https://playwright.dev/docs/intro')
 })
+
+// file level (test config )> projects level > global level
